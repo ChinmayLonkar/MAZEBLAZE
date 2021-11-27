80f9bb32a1303c8269265a200f6dfe4977dc21f8
ChinmayLonkar <chinmay20220@gmail.com>
2021-11-27 13:56:57 +0530

    Setup for White and Black margins

--- firmware/1_lsa/main/main.c
+++ firmware/1_lsa/main/main.c
@@ -4,10 +4,11 @@
 void app_main(void)
 {
     ESP_ERROR_CHECK(enable_lsa());
-
+    ESP_ERROR_CHECK(enable_motor_driver());
+   
     while (1)
     {
-        printf("%d %d %d %d %d \n", get_reading_lsa().lsa_reading[0], get_reading_lsa().lsa_reading[1], get_reading_lsa().lsa_reading[2], get_reading_lsa().lsa_reading[3],get_reading_lsa().lsa_reading[4]);
-        vTaskDelay(100 / portTICK_RATE_MS);
+        printf("%d %d %d %d %d \n", read_lsa().lsa_read[0], read_lsa().lsa_read[2], read_lsa().lsa_read[1], read_lsa().lsa_read[4],read_lsa().lsa_read[3]);
+        vTaskDelay(10 / portTICK_RATE_MS);
     }
 }

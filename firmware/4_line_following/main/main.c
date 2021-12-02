1a2be2e92abab6bb84327b5d0c494900c221e1c4
ChinmayLonkar <chinmay20220@gmail.com>
2021-12-02 16:12:57 +0530

    CMake list changed

--- firmware/4_line_following/main/main.c
+++ firmware/4_line_following/main/main.c
@@ -1,61 +1,59 @@
 #include "mazeblaze.h"
-#include "tuning_http_server.h"
+//#include "tuning_http_server.h"
 #include "line_following.h"
 #include "node_detection.h"
 #include "turn.h"
-#include "esp_log.h"
+//#include "esp_log.h"
 #include "esp_err.h"
-#include "wifi_logger.h"
-
+// #include "wifi_logger.h"
 void maze_explore(void *arg)
 {
-    ESP_ERROR_CHECK(enable_lsa());
-    ESP_ERROR_CHECK(enable_motor_driver());
-
+   
     while (1)
     {
         if (only_straight())
         {
             go_straight();
         }
-        // else if (only_left())
-        // {
-        //     turn(LEFT);
-        // }
-        // else if (only_right())
-        // {
-        //     turn(RIGHT);
-        // }
+        else if (only_left())
+        {
+            turn(LEFT);
+        }
+        else if (only_right())
+        {
+            turn(RIGHT);
+        }
 
-        // else if (plus_node())
-        // {
-        //     turn(LEFT);
-        // }
+        else if (plus_node())
+        {
+            turn(LEFT);
+        }
 
-        // else if (T_node())
-        // {
-        //     turn(LEFT);
-        // }
+        else if (T_node())
+        {
+            turn(LEFT);
+        }
 
-        // else if (straight_right())
-        // {
-        //     go_straight();
-        // }
+        else if (straight_right())
+        {
+            go_straight();
+        }
 
-        // else if (straight_left())
-        // {
-        //     turn(LEFT);
-        // }
+        else if (straight_left())
+        {
+            turn(LEFT);
+        }
 
-        // else if (deadend())
-        // {
-        //     turn(UTURN);
-        // }
+        else if (deadend())
+        {
+            turn(UTURN);
+        }
 
-        // else if (end())
-        // {
-        //     stop();
-        // }
+        else if (end())
+        {
+            stop();
+            vTaskDelete(NULL);
+        }
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
@@ -63,12 +61,15 @@ void maze_explore(void *arg)
 
 void app_main()
 {
-    // xTaskCreate(&maze_explore, "maze_explore", 4096, NULL, 1, NULL);
-    start_wifi_logger(); // Start wifi logger
-    start_tuning_http_server();
-    while (1)
-    {
-        wifi_log_e("test", "%s %d %f", "hello world wifi logger", 43, 45.341223242); // write log over wifi with log level -> ERROR
-        vTaskDelay(10);
-    }
+    ESP_ERROR_CHECK(enable_lsa());
+    ESP_ERROR_CHECK(enable_motor_driver());
+
+    xTaskCreate(&maze_explore, "maze_explore", 4096, NULL, 1, NULL);
+    //start_wifi_logger(); // Start wifi logger
+    //start_tuning_http_server();
+    // while (1)
+    // {
+    //     wifi_log_e("test", "%s %d %f", "hello world wifi logger", 43, 45.341223242); // write log over wifi with log level -> ERROR
+    //     vTaskDelay(10);
+    // }
 }

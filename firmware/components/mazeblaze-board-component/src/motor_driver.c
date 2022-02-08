f32e18f7cc065eff4aec3bd6fd791553febbea59
ChinmayLonkar <chinmay20220@gmail.com>
2022-02-08 11:53:25 +0530

    PWM pin issue resolved

--- firmware/components/mazeblaze-board-component/src/motor_driver.c
+++ firmware/components/mazeblaze-board-component/src/motor_driver.c
@@ -5,10 +5,10 @@ static const char *TAG_MOTOR_DRIVER = "motor_driver";
 esp_err_t enable_motor_driver()
 {
     esp_err_t err;
-        CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM0A, MDA_NORMAL_IN_3), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
-        CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM0B, MDA_NORMAL_IN_4), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
-        CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM1A, MDA_NORMAL_IN_5), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
-        CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM1B, MDA_NORMAL_IN_6), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
+    CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM0A, MDA_NORMAL_IN_3), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
+    CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM0B, MDA_NORMAL_IN_4), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
+    CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM1A, MDA_NORMAL_IN_5), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
+    CHECK_LOGE(err, mcpwm_gpio_init(MCPWM_UNIT_1, MCPWM1B, MDA_NORMAL_IN_6), TAG_MOTOR_DRIVER, "error: %s", esp_err_to_name(err));
 
     mcpwm_config_t pwm_config;
     pwm_config.frequency = 20000;
@@ -19,6 +19,14 @@ esp_err_t enable_motor_driver()
     esp_err_t err1_0 = mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_0, &pwm_config);
     esp_err_t err1_1 = mcpwm_init(MCPWM_UNIT_1, MCPWM_TIMER_1, &pwm_config);
 
+    gpio_config_t o_conf;
+    o_conf.intr_type = 0;
+    o_conf.mode = GPIO_MODE_OUTPUT;
+    o_conf.pin_bit_mask = ((1ULL << GPIO_NUM_33) | (1ULL <<GPIO_NUM_12));
+    o_conf.pull_down_en = 0;
+    o_conf.pull_up_en = 0;
+    
+    gpio_config(&o_conf);
     if (err1_0 == ESP_OK && err1_1 == ESP_OK)
     {
         ESP_LOGI(TAG_MOTOR_DRIVER, "enabled motor");
@@ -30,12 +38,12 @@ esp_err_t enable_motor_driver()
     }
 }
 
-
-
 static esp_err_t set_motor_speed_helper(int direction, float duty_cycle, mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num)
 {
-    if (direction == MOTOR_FORWARD)
+    if (direction == MOTOR_BACKWARD)
     {
+        gpio_set_level(GPIO_NUM_12, 1);
+        gpio_set_level(GPIO_NUM_33, 1);
         mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, duty_cycle);
         mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0);
         mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, 0);
@@ -43,8 +51,10 @@ static esp_err_t set_motor_speed_helper(int direction, float duty_cycle, mcpwm_u
 
         return ESP_OK;
     }
-    else if (direction == MOTOR_BACKWARD)
+    else if (direction == MOTOR_FORWARD)
     {
+        gpio_set_level(GPIO_NUM_12, 1);
+        gpio_set_level(GPIO_NUM_33, 1);
         mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, 0);
         mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0);
         mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, duty_cycle);
@@ -54,9 +64,11 @@ static esp_err_t set_motor_speed_helper(int direction, float duty_cycle, mcpwm_u
     }
     else if (direction == MOTOR_STOP)
     {
-        mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, 0);
+        gpio_set_level(GPIO_NUM_12, 0);
+        gpio_set_level(GPIO_NUM_33, 0);
+        mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_A, 100);
         mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_A, MCPWM_DUTY_MODE_0);
-        mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, 0);
+        mcpwm_set_duty(mcpwm_num, timer_num, MCPWM_OPR_B, 100);
         mcpwm_set_duty_type(mcpwm_num, timer_num, MCPWM_OPR_B, MCPWM_DUTY_MODE_0);
 
         return ESP_OK;
@@ -70,8 +82,6 @@ static esp_err_t set_motor_speed_helper(int direction, float duty_cycle, mcpwm_u
 
 esp_err_t set_motor_speed(int motor_id, int direction, float duty_cycle)
 {
-   mcpwm_timer_t timer_val = motor_id == MOTOR_A_0 ? MCPWM_TIMER_0 : MCPWM_TIMER_1;
-   return set_motor_speed_helper(direction, duty_cycle, MCPWM_UNIT_1, timer_val);
+    mcpwm_timer_t timer_val = motor_id == MOTOR_A_0 ? MCPWM_TIMER_0 : MCPWM_TIMER_1;
+    return set_motor_speed_helper(direction, duty_cycle, MCPWM_UNIT_1, timer_val);
 }
-
-

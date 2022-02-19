f49c9b5d5a925cb510e93d3ed4e9b666240134f5
ChinmayLonkar <chinmay20220@gmail.com>
2022-02-19 13:49:51 +0530

    Merged loops

--- firmware/4_line_following/main/main.c
+++ firmware/4_line_following/main/main.c
@@ -17,9 +17,10 @@
 bool endl = true;
 int palat;
 int prev = 0;
-
+int i = 0;
+char actual_path[] = "RRLRLLRLRRSRRLSLRLLSLRSST";
 int duty_cycle = 72;
-int _turn;
+int _turn, end = 0;
 bool l = false, r = false, pt = false, ot = false, flag = false;
 float Kp = 5;
 float Ki = 0.5;
@@ -29,9 +30,8 @@ float error = 0, P = 0, I = 0, D = 0, PID_value = 0;
 float previous_error = 0, previous_I = 0;
 void maze_explore(void *arg)
 {
-    while (1)
+    while (gpio_get_level(DEBUG_SWITCH) == 1)
     {
-
         while (1)
         {
             if ((read_lsa().data[1] == 1) && (read_lsa().data[2] == 0) && (read_lsa().data[3] == 0))
@@ -57,6 +57,10 @@ void maze_explore(void *arg)
             prev++;
             if (((read_lsa().data[4] == 1) || (read_lsa().data[0] == 1)) && (prev >= 50))
             {
+                if (read_lsa().data[0] == 1)
+                {
+                    end++;
+                }
                 prev = 0;
                 break;
             }
@@ -92,11 +96,13 @@ void maze_explore(void *arg)
         if (pt)
         {
             _turn = T;
+            end = 0;
             palat = LEFT;
         }
         if (l)
         {
             _turn = L;
+            end = 0;
             palat = LEFT;
         }
         if (r)
@@ -104,6 +110,7 @@ void maze_explore(void *arg)
             if (read_lsa().data[2] == 0)
             {
                 _turn = R;
+                end = 0;
                 palat = RIGHT;
             }
             else if (read_lsa().data[2] == 1)
@@ -111,11 +118,17 @@ void maze_explore(void *arg)
                 _turn = ST_R;
             }
         }
+        if (end >= 30)
+        {
+            end = 0;
+            _turn = ST_R;
+            set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
+            set_motor_speed(MOTOR_A_1, MOTOR_STOP, 0);
+            set_led_on();
+        }
         l = false;
         r = false;
         pt = false;
-        // ESP_LOGI("debug", "Turn: %d", _turn);
-        // ESP_LOGI("debug", "Palat: %d", palat);
 
         if (_turn != ST_R)
         {
@@ -123,6 +136,88 @@ void maze_explore(void *arg)
             prev = 0;
             take_turn(palat);
         }
+        vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+    while (gpio_get_level == 0)
+    {
+
+        while (1)
+        {
+            if ((read_lsa().data[1] == 1) && (read_lsa().data[2] == 0) && (read_lsa().data[3] == 0))
+                error = -2;
+            else if ((read_lsa().data[1] == 1) && (read_lsa().data[2] == 1) && (read_lsa().data[3] == 0))
+                error = -1;
+            else if ((read_lsa().data[1] == 0) && (read_lsa().data[2] == 1) && (read_lsa().data[3] == 0))
+                error = 0;
+            else if ((read_lsa().data[1] == 0) && (read_lsa().data[2] == 1) && (read_lsa().data[3] == 1))
+                error = 1;
+            else if ((read_lsa().data[1] == 0) && (read_lsa().data[2] == 0) && (read_lsa().data[3] == 1))
+                error = 2;
+            P = error;
+            I = I + previous_I;
+            D = error - previous_error;
+            PID_value = (Kp * P) + (Ki * I) + (Kd * D);
+            previous_I = I;
+            previous_error = error;
+            speed_A_0 = duty_cycle - PID_value;
+            speed_A_1 = duty_cycle + PID_value;
+            set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, speed_A_0);
+            set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, speed_A_1);
+            prev++;
+            if (((read_lsa().data[4] == 1) || (read_lsa().data[0] == 1)) && (prev >= 50))
+            {
+                prev = 0;
+                break;
+            }
+            // if ((read_lsa().data[1] == 1) && (read_lsa().data[2] == 0) && (read_lsa().data[3] == 1))
+            // {
+            //     _turn = U;
+            //     palat = UTURN;
+            //     break;
+            // }
+        }
+        if (read_lsa().data[4] == 1 || read_lsa().data[0] == 1)
+        {
+            vTaskDelay(50 / portTICK_PERIOD_MS);
+
+            while (i <= 100)
+            {
+                ESP_LOGI("debug", "PATH: %d", i);
+
+                if (actual_path[i] == 'R')
+                {
+                    prev = 0;
+                    take_turn(RIGHT);
+                    i++;
+                    break;
+                }
+                else if (actual_path[i] == 'L')
+                {
+                    take_turn(LEFT);
+                    prev = 0;
+                    i++;
+
+                    break;
+                }
+                else if (actual_path[i] == 'S')
+                {
+                    take_turn(STRAIGHT);
+                    prev = 0;
+                    i++;
+
+                    break;
+                }
+                else if (actual_path[i] == 'T')
+                {
+                    set_led_on();
+                    set_motor_speed(MOTOR_A_0, MOTOR_STOP, 0);
+                    set_motor_speed(MOTOR_A_1, MOTOR_STOP, 0);
+                    break;
+                }
+            }
+        }
+        // ESP_LOGI("debug", "Turn: %d", _turn);
+        // ESP_LOGI("debug", "Palat: %d", palat);
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
